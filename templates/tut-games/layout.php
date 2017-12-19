c9fc2a44fffb5320d3441d578e3800b8327636f4
SergeiVL <sergeivl39@gmail.com>
2017-12-19 23:30:22 +0200

    коррект лого

--- templates/tut-games/layout.php
+++ templates/tut-games/layout.php
@@ -42,7 +42,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="/"><span style="color:#5cb85c;">Unik</span>Games.Ru</a>
+            <a class="navbar-brand" href="/"><span style="color:#5cb85c;">Tut</span>-Games.Ru</a>
         </div>
         <div id="navbar" class="collapse navbar-collapse">
             <ul class="nav navbar-nav">
