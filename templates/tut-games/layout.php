e9a24ab30f71633097b38e4963e58814476eb5c6
SergeiVL <sergeivl39@gmail.com>
2017-12-19 23:38:01 +0200

    Счетчик

--- templates/tut-games/layout.php
+++ templates/tut-games/layout.php
@@ -70,7 +70,16 @@ require '_' . $subtemplate . '.php' ?>
             <a href="http://tut-games.ru/">Tut-Games.Ru</a> - скачать игры на русском бесплатно через торрент
         </div>
         <div class="col-xs-6" style="text-align:right;">
-
+            <!--LiveInternet counter--><script type="text/javascript"><!--
+                document.write("<a href='//www.liveinternet.ru/click' "+
+                    "target=_blank><img src='//counter.yadro.ru/hit?t44.5;r"+
+                    escape(document.referrer)+((typeof(screen)=="undefined")?"":
+                        ";s"+screen.width+"*"+screen.height+"*"+(screen.colorDepth?
+                        screen.colorDepth:screen.pixelDepth))+";u"+escape(document.URL)+
+                    ";"+Math.random()+
+                    "' alt='' title='LiveInternet' "+
+                    "border='0' width='31' height='31'><\/a>")
+                //--></script><!--/LiveInternet-->
 
         </div>
     </div>
