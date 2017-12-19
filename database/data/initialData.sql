3d7ab23c879963c0d5a13874a3bb31645ef416a5
sergeivl <sergeivl39@gmail.com>
2017-12-19 10:37:45 +0200

    Фикс миграции данных

--- database/data/initialData.sql
+++ database/data/initialData.sql
@@ -1,2 +1,2 @@
 INSERT INTO `fp_pages` (`id`, `title`, `title_seo`, `meta_d`, `text`, `alias`, `status`, `created_at`, `updated_at`) VALUES
-(1,	'Главная страница сайта',	'Заголовок сайта',	'Описание сайта',	'Добро пожаловать на наш сайт',	'main',	'published',	now(),	NULL);
+(1,	'Главная страница сайта',	'Заголовок сайта',	'Описание сайта',	'Добро пожаловать на наш сайт',	'main',	1,	now(),	NULL);
