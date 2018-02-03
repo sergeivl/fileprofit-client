5113a42f3ff516f86c5d430d720cb3b44e4d41fb
SergeiVL <sergeivl39@gmail.com>
2018-02-03 16:08:50 +0200

    Сэмпл для кнопки

---
+++ templates/standart/_downloadButton.sample.php
@@ -0,0 +1,7 @@
+<?php
+/** @var App\Models\Game $game */
+?>
+
+<div style="margin-top: 8px; width: 200px; text-align: center;">
+    <a style="color:forestgreen;" href="<?= $game->torrent ?>">[Торрент <?= $game->fileSize ?>]</a>
+</div>
